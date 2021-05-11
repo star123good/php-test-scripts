c988c35ecae629e8693f7b60dc09d31016585713
star123good <star921str@hotmail.com>
2021-05-12 03:54:32 +0800

    remove some folders

--- gotee/gotee/scripts/tosin_script.php
+++
@@ -1,8 +0,0 @@
-<?php
-#!/usr/bin/env php
-    $name = 'Oladapo Tosin';
-    $id = 'NGI0013';
-    $language = 'PHP';
-    
-    print 'Hello World, this is '. $name . ' with '. $id . '[ID] using '.$language.' for stage 2 task';
-   
